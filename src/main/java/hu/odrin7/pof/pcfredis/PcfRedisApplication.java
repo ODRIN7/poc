1f05c81f93eacba6f75299260a511ac6ea9b716f
odrin7 <sz.danix6@gmail.com>
2018-10-12 12:24:48 +0200

    swagger

--- src/main/java/hu/odrin7/pof/pcfredis/PcfRedisApplication.java
+++ src/main/java/hu/odrin7/pof/pcfredis/PcfRedisApplication.java
@@ -1,16 +1,7 @@
 package hu.odrin7.pof.pcfredis;
 
-import hu.odrin7.pof.pcfredis.entity.ObjectReference;
-import hu.odrin7.pof.pcfredis.entity.PaymentIdEntity;
-import hu.odrin7.pof.pcfredis.entity.RelationEntity;
-import hu.odrin7.pof.pcfredis.repostiory.PaymentIdRepository;
-import hu.odrin7.pof.pcfredis.repostiory.RelationRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
